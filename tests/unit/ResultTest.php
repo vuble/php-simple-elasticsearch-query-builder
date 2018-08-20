fdeb23794ff4a032d216d27301200768856ba9cf
Geoffray Veyssié <gv@mediabong.com>
2018-08-20 11:10:53 +0200

    Mod: remove us eof file for nested unit test

--- tests/unit/ResultTest.php
+++ tests/unit/ResultTest.php
@@ -44,14 +44,89 @@ class ResultTest extends \PHPUnit_Framework_TestCase
     public function test_getAsSqlResult_NestedAggregations()
     {
         $result = (new ElasticSearchResult(
-            require_once __DIR__.'/../data/ElasticSearchResultTest_ES_Result_Nested.php'
+            [
+                'hits' => [
+                    'total' => 12348,
+                    'max_score' => 1,
+                    'hits' => []
+                ],
+                'aggregations' => [
+                    'group_by_type' => [
+                        'buckets' => [
+                            0 => [
+                                'key' => 'request',
+                                'doc_count' => 12349,
+                                'group_by_id' => [
+                                    'buckets' => [
+                                        0 => [
+                                            'key' => 529,
+                                            'doc_count' => 12347,
+                                            'nested_deals' => [
+                                                'doc_count' => 47288,
+                                                'filter_deal_id' => [
+                                                    'doc_count' => 47288,
+                                                    'group_by_deals.deal_id' => [
+                                                        'buckets' => [
+                                                            0 => [
+                                                                'key' => 'DEAL1',
+                                                                'doc_count' => 12349,
+                                                            ],
+                                                            1 => [
+                                                                'key' => 'DEAL2',
+                                                                'doc_count' => 12346,
+                                                            ],
+                                                            2 => [
+                                                                'key' => 'DEAL3',
+                                                                'doc_count' => 12347,
+                                                            ],
+                                                            3 => [
+                                                                'key' => 'DEAL4',
+                                                                'doc_count' => 9974,
+                                                            ],
+                                                        ],
+                                                    ],
+                                                ],
+                                            ],
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ]
         ))
         ->getAsSqlResult()
         ;
 
         $this->assertEquals(
             $result,
-            require_once __DIR__.'/../data/ElasticSearchResultTest_ES_Result_As_Sql_Nested.php'
+            [
+                'type:request-id:529-deals.deal_id:DEAL1' => [
+                    'type' => 'request',
+                    'id' => 529,
+                    'deals.deal_id' => 'DEAL1',
+                    'total' => 12347,
+                ],
+                'type:request-id:529-deals.deal_id:DEAL2' => [
+                    'type' => 'request',
+                    'id' => 529,
+                    'deals.deal_id' => 'DEAL2',
+                    'total' => 12346,
+                ],
+                'type:request-id:529-deals.deal_id:DEAL3' => [
+                    'type' => 'request',
+                    'id' => 529,
+                    'deals.deal_id' => 'DEAL3',
+                    'total' => 12347,
+                ],
+                'type:request-id:529-deals.deal_id:DEAL4' => [
+                    'type' => 'request',
+                    'id' => 529,
+                    'deals.deal_id' => 'DEAL4',
+                    'total' => 9974,
+                ],
+            ]
         );
     }
 
